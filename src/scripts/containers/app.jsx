0d96bcc1900373e644269e77b2aa7014f4acae57
Daniel Levitt <daniel.levitt@guardian.co.uk>
2015-10-28 09:28:48 +0000

    Remove RX

--- src/scripts/containers/app.jsx
+++ src/scripts/containers/app.jsx
@@ -1,15 +1,8 @@
 import React from "react";
 import Reflux from "reflux";
-import Rx from "rx";
 import { Settings, Sounds, Theme } from "../stores";
 import { Nav, SoundList } from "../components";
 import "../styles/base.css";
-
-export const Konami = Rx.Observable.fromEvent(window, "keyup")
-  .map(el => el.keyCode)
-  .windowWithCount(10, 1)
-  .selectMany(_x => _x.toArray())
-  .filter(seq => seq.toString() === [38, 38, 40, 40, 37, 39, 37, 39, 66, 65].toString());
 
 export default React.createClass({
   propTypes: {
@@ -17,13 +10,7 @@ export default React.createClass({
   },
   mixins: [ Reflux.connect(Sounds, "sounds"), Reflux.connect(Theme, "theme"), Reflux.connect(Settings, "settings") ],
   getInitialState() {
-    return { konami: false };
-  },
-  componentDidMount() {
-    Konami.subscribe(this.konamiEntered);
-  },
-  konamiEntered() {
-    this.setState({ konami: !this.state.konami });
+    return {};
   },
   render() {
     return (
