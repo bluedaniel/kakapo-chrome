f65ae64563c8de645a7af6835b599296bb305693
Daniel Levitt <daniel.levitt@guardian.co.uk>
2015-10-28 08:47:19 +0000

    Remove unneeded files

--- src/scripts/components/ui/index.js
+++ src/scripts/components/ui/index.js
@@ -1,4 +1,1 @@
-export { default as ColorPicker } from "./colorPicker/colorPicker";
 export { default as Image } from "./image/image";
-export { default as ProgressBar } from "./progressBar/progressBar";
-export { default as Checkbox } from "./checkbox/checkbox";
